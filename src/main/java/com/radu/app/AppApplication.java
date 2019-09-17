05beaa8f06caa8965689285fdfba746827bde2e9
radudiaconu0 <52667211+radudiaconu0@users.noreply.github.com>
2019-09-17 13:49:53 +0300

    added files
    added file

---
+++ src/main/java/com/radu/app/AppApplication.java
@@ -0,0 +1,21 @@
+package com.radu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class AppApplication {
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "Hello";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+
+}
